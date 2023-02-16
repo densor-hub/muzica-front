a5a8dcefd8e00ef46af147021999a16d757cce72
densor-hub <60878390+densor-hub@users.noreply.github.com>
2023-02-15 23:51:08 -0700

    Update BaseURL.js

--- src/Resources/BaseURL.js
+++ src/Resources/BaseURL.js
@@ -1,5 +1,5 @@
 
-const local = 'http://localhost:3500';
-//const deployed = 'http://ec2-3-239-233-231.compute-1.amazonaws.com:3500';
+//const local = 'http://localhost:3500';
+const deployed = 'http://ec2-3-239-233-231.compute-1.amazonaws.com:3500';
 
-export const API_BASE_URL = local;
+export const API_BASE_URL = deployed ;
